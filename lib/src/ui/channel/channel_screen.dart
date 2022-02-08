75155a855f753c51b5e7041571240759e7eed1cc
thurain <thurainbowin.dev@gmail.com>
2022-02-08 19:00:01 +0630

    - build update

---
+++ lib/src/ui/channel/channel_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ChannelScreen extends StatefulWidget {
+  const ChannelScreen({ Key? key }) : super(key: key);
+
+  @override
+  _ChannelScreenState createState() => _ChannelScreenState();
+}
+
+class _ChannelScreenState extends State<ChannelScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Channel"),
+      ),
+    );
+  }
+}
--- lib/src/ui/users/users_screen.dart
+++ lib/src/ui/users/users_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 import 'package:wisper/src/network/models/demo_user.dart';
+import 'package:wisper/src/ui/channel/channel_screen.dart';
 
 var demoUserList = [
   DemoUser(
@@ -22,12 +24,80 @@ class UsersScreen extends StatefulWidget {
 }
 
 class _UsersScreenState extends State<UsersScreen> {
+  bool _isLoading = false;
+
+  /// Method to connect a user.
+  Future<void> _connectUser(DemoUser demoUser) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final client = StreamChat.of(context).client;
+    try {
+      await client.connectUser(
+        User(
+          id: demoUser.id,
+          name: demoUser.name,
+          image: demoUser.image,
+        ),
+        client.devToken(demoUser.id).rawValue,
+      );
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const ChannelScreen()),
+      );
+    } on Exception catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Users'),
+      body: SafeArea(
+        child: _isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Column(
+                children: [
+                  Text(
+                    'Select a User',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        ...demoUserList.map((u) => UserTile(
+                              user: u,
+                              callback: _connectUser,
+                            )),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
       ),
+    );
+  }
+}
+
+class UserTile extends StatelessWidget {
+  final Function callback;
+  final DemoUser user;
+
+  UserTile({required this.user, required this.callback});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        foregroundImage: Image.network(user.image).image,
+      ),
+      title: Text(user.name),
+      onTap: () => callback(user),
     );
   }
 }
